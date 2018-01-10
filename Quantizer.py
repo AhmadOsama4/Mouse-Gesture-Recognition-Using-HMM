6ddc3e3c1fa90cc015deff67341a635cca8b7948
Ahmad Osama <ahmad.osama.moh@gmail.com>
2018-01-10 13:06:11 +0200

    Adding Quantizer

---
+++ Quantizer.py
@@ -0,0 +1,20 @@
+import math
+
+# levels => number of quantized levels
+def quantize(X, Y, levels):
+	observations = []
+	quantizing_angle = 2 * math.pi / levels
+
+	for i in range(1, len(X)):
+		dx = X[i] - X[i - 1]
+		dy = Y[i] - Y[i - 1]
+
+		theta = math.atan2(dy, dx)
+		if theta < 0:
+			theta += 2 * math.pi
+
+		level = math.floor(theta / quantizing_angle)
+
+		observations.append(int(level))
+
+	return observations
