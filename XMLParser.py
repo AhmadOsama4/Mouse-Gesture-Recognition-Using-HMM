b12bf99fd3fdb5ae01d60ed402c122e9c17018a4
Ahmad Osama <ahmad.osama.moh@gmail.com>
2018-01-10 11:35:45 +0200

    Modify XML Parser

--- XMLParser.py
+++ XMLParser.py
@@ -19,16 +19,8 @@ def parseFile(filename):
 			y = point.find('Y').text
 			xpoints.append(x)
 			ypoints.append(y)
-			#print x + " " + y
+
 		X.append(xpoints)
 		Y.append(ypoints)
 
 	return X, Y
-
-#X, Y = parseFile("TESTGesture.xml")
-
-#for i in range(len(X)):
-#	print "Sequence: "
-#	for j in range(len(X[i])):
-#		print X[i][j] + ' ' + Y[i][j]
-#	print ''
