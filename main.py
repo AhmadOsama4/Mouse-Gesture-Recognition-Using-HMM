522ac958a745fe7f0fd76fe5b35b3487cf00ffd8
Ahmad Osama <ahmad.osama.moh@gmail.com>
2018-01-12 00:06:44 +0200

    Testing on Training data

--- main.py
+++ main.py
@@ -13,17 +13,28 @@ classifier.train()
 
 seq1 = getSequences('BottomLeftCornerGestures', emissions)
 
+correct = 0
+total = 0
+
 for seq in seq1:
 	result = classifier.predict(seq)
 	if result[0] == 'B':
 		print 'Class 1 Correct'
+		correct += 1
 	else:
 		print 'Class 1 Wrong'
+	total += 1
 
 seq2 = getSequences('TESTGesture', emissions)
 for seq in seq2:
 	result = classifier.predict(seq)
 	if result[0] == 'T':
 		print 'Class 2 Correct'
+		correct += 1
 	else:
 		print 'Class 2 Wrong'
+	total += 1
+
+accuracy = 1.0*correct / total
+
+print 'Accuracy ', accuracy
